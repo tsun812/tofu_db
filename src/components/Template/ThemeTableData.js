d4936d596ea2ff056f8ca4200e1bab8425053ffb
Yan Sun <tsun812@gmail.com>
2022-02-21 12:45:25 -0800

    sort and search in table view

--- src/components/Template/ThemeTableData.js
+++ src/components/Template/ThemeTableData.js
@@ -1,16 +1,35 @@
 import React from "react";
-import "./ThemeTable.scss"
+import "./ThemeTable.scss";
 import ThemeTableDataItem from "./ThemeTableDataItem";
-export default function ThemeTableData({tableDataArray}) {
-  return (
-    Object.keys(tableDataArray).map((rowData, index) => {
-      return <tr key={"row", tableDataArray[rowData].id}>
+export default function ThemeTableData(props) {
+  // return (
+  //   Object.keys(tableDataArray).map((rowData, index) => {
+  //     return <tr key={"row", tableDataArray[rowData].id}>
+  //       <td className="indexNum">{index + 1}</td>
+  //       <ThemeTableDataItem
+  //         key={tableDataArray[rowData].id}
+  //         rowData={tableDataArray[rowData].values}
+  //       />
+  //       </tr>
+  //   })
+  // )
+  console.log(props.filteredRecords);
+  let recordsRender;
+  if (
+    Array.isArray(props.filteredRecords) &&
+    props.filteredRecords.length > 0
+  ) {
+    recordsRender = props.filteredRecords;
+  } else {
+    recordsRender = props.selectedRecords;
+  }
+  return recordsRender.map((rowData, index) => {
+    return (
+      <tr key={("row", rowData.id)}>
         <td className="indexNum">{index + 1}</td>
-        <ThemeTableDataItem
-          key={tableDataArray[rowData].id}
-          rowData={tableDataArray[rowData].values}
-        />
-        </tr>
-    })
-  )
+        <td key={index * 2}> {rowData.primary} </td>
+        <td key={index * 2 + 1}> {rowData.secondary} </td>
+      </tr>
+    );
+  });
 }
