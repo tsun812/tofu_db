b36915364560ce47cfad84bc98bc8de7e7bfc379
samsam3268 <samskleung@gmail.com>
2022-02-20 23:32:10 -0800

    improve login layout

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,32 +1,38 @@
 import React from "react";
+import "components/login/Login.scss";
 export default function Login(props) {
-  const login = () => {props.setMode("Data")}
+  const login = () => { props.setMode("Data") }
   return (
-    <div className="login">
-      <img src="https://media.istockphoto.com/photos/block-and-cubes-of-tofu-picture-id465944034?k=20&m=465944034&s=612x612&w=0&h=OCejoEitg8O0ChNb3Kg40-dpOY4SUO7atNCFKZmpqKQ="
-   width="200" height="180"/>
-      <form
-        className="login-form"
-        method="post"
-        action="/login/"
-      ></form>
-      <h1 className="h3 mb-3 font-weight-normal">Into the TOFU DB</h1>
-      <label for="inputEmail" class="sr-only">
-        Email address
-      </label>
-      <input
-        type="email"
-        id="inputEmail"
-        class="form-control"
-        placeholder="Email address"
-        required
-        autofocus
-      ></input>
-      <label for="inputPassword" class="sr-only">Password</label>
-      <input type="password" id="inputPassword" class="form-control" placeholder="Password" required></input>
-      <div class="checkbox mb-3">
-    </div>
-    <button class="btn btn-lg btn-primary btn-block" type="submit" onClick={login}>Sign in</button>
+    <div className="loginContainer">
+      <div className="login">
+        <div className="tofuImageContainer">
+          <img className="tofuImage" src="https://media.istockphoto.com/photos/block-and-cubes-of-tofu-picture-id465944034?k=20&m=465944034&s=612x612&w=0&h=OCejoEitg8O0ChNb3Kg40-dpOY4SUO7atNCFKZmpqKQ=" />
+        </div>
+        <form
+          className="login-form"
+          method="post"
+          action="/login/"
+        ></form>
+        <h1 className="loginTitle">Into the TOFU DB</h1>
+        <label for="inputEmail" class="sr-only">
+          Email address
+        </label>
+        <input
+          type="email"
+          id="inputEmail"
+          class="form-control"
+          placeholder="Email address"
+          required
+          autofocus
+        ></input>
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required></input>
+        <div class="checkbox mb-3">
+        </div>
+        <div class="signInButtonContainer">
+          <button class="btn btn-lg btn-primary btn-block signInButton" type="submit" onClick={login}>Sign in</button>
+        </div>
+      </div>
     </div>
   );
 }
